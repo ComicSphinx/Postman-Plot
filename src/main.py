dac6b06173debf0f8cb2a6f8bf911af7e6592e40
Desasth <invicems@gmail.com>
2021-03-24 13:20:47 +0400

    added main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+# @Author: Daniil Maslov (ComicSphinx)
+
+import json
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+with open('test_result.json', 'r') as file:
+    text = json.load(file)
+
+passed = text['totalPass']
+failed = text['totalFail']
+tests_labels = ['passed', 'failed']
+yticks = [passed, failed]
+
+tests = [passed, failed]
+
+mpl.rcParams['toolbar'] = 'none'
+plt.ylabel("Quantity")
+plt.yticks(yticks)
+plt.bar(tests_labels, tests, align='center', width=0.3)
+plt.subplots_adjust(bottom = 0.1, top = 0.95)
+
+plt.show()
